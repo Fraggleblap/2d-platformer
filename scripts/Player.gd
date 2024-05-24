2e787e3ca6e034df720caf21b39c6104daafd42f
LilytheIrishLass <Owen.Gaffney23@student.cart.org>
2024-05-23 18:15:51 -0700

    More assets added

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,44 +1,37 @@
 extends CharacterBody2D
 
+const SPEED = 90.0
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var game_manager = %GameManager
 
-
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	# Remove gravity.
+	# if not is_on_floor():
+	#     velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Remove jump handling.
+	# if Input.is_action_just_pressed("jump") and is_on_floor():
+	#     velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
+	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	var vertical_direction = Input.get_axis("move_up", "move_down")
 	
-	if direction > 0:
+	# Horizontal movement and animation.
+	if horizontal_direction > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif horizontal_direction < 0:
 		animated_sprite.flip_h = true
 	
-	if is_on_floor():
-		if direction ==0:
-			animated_sprite.play("idle")	
-		else:
-			animated_sprite.play("run")
+	if horizontal_direction != 0 or vertical_direction != 0:
+		animated_sprite.play("run")
 	else:
-		animated_sprite.play("jump")
+		animated_sprite.play("idle")
 	
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Set velocity based on input direction.
+	velocity.x = horizontal_direction * SPEED
+	velocity.y = vertical_direction * SPEED
+
 	move_and_slide()
