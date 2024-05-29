ce8d64050e619b456721037f81ab3b1500bc1d45
Fraggleblap <Owen.Gaffney23@student.cart.org>
2024-05-29 14:43:10 -0700

    v0.0.6
    Interaction system

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var game_manager = %GameManager
 @export var speed = 90
 
+
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
@@ -20,3 +21,14 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	if get_node("RayCast2D_R").is_colliding():
+		var object = get_node("RayCast2D_R").get_collider()
+		if object.is_in_group("Interactable"):
+			if Input.is_action_just_pressed("interact"):
+				print("iunteractededd")
+				print(object)
+	elif get_node("RayCast2D_L").is_colliding():
+		var object = get_node("RayCast2D_L").get_collider()
+		if object.is_in_group("Interactable"):
+			if Input.is_action_just_pressed("interact"):
+				print("iunteracteded12312321d")
--- scripts/interact_object.gd
+++ scripts/interact_object.gd
@@ -1,8 +1,3 @@
 extends Area2D
 
 @onready var interact_collision = $interactCollision
-
-
-
-func _on_interact_collision_tree_entered():
-	pass # Replace with function body.
